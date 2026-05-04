df0dfccb40c83d7ea7162ec149609a8d3af13579
akayrailhan <alikayra.ilhannn@gmail.com>
2026-05-04 22:26:16 +0300

    feat(observability): add Serilog logging and Prometheus metrics

--- TicketAPI/Program.cs
+++ TicketAPI/Program.cs
@@ -1,44 +1,68 @@
 using MediatR;
 using TicketAPI.Data;
 using Microsoft.EntityFrameworkCore;
+using Prometheus;
+using Serilog;
 using StackExchange.Redis;
 
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateLogger();
 
-var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = builder.Configuration.GetConnectionString("Supabase");
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(connectionString));
-
-var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
-
-var configurationOptions = ConfigurationOptions.Parse(redisConnectionString!);
-// Yerel bilgisayardaki Firewall/SSL engellerini aşmak için kritik ayar:
-configurationOptions.CertificateValidation += delegate { return true; };
-
-builder.Services.AddStackExchangeRedisCache(options =>
+try
 {
-    options.ConfigurationOptions = configurationOptions;
-    options.InstanceName = "TicketAPI_";
-});
+    Log.Information("Uygulama baslatiliyor...");
 
-builder.Services.AddMediatR(typeof(Program));
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+    var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+    builder.Host.UseSerilog();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var connectionString = builder.Configuration.GetConnectionString("Supabase");
+
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(connectionString));
+
+    var redisConnectionString = builder.Configuration.GetConnectionString("Redis");
+
+    var configurationOptions = ConfigurationOptions.Parse(redisConnectionString!);
+    // Yerel bilgisayardaki Firewall/SSL engellerini asmak icin kritik ayar:
+    configurationOptions.CertificateValidation += delegate { return true; };
+
+    builder.Services.AddStackExchangeRedisCache(options =>
+    {
+        options.ConfigurationOptions = configurationOptions;
+        options.InstanceName = "TicketAPI_";
+    });
+
+    builder.Services.AddMediatR(typeof(Program));
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseHttpMetrics();
+
+    app.MapControllers();
+
+    app.MapMetrics();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Uygulama beklenmedik bir sekilde coktu!");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
